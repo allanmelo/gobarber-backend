445e158718f2024fc82674b9eb13e5c0e9376fdb
Allan Melo <allmelo@users.noreply.github.com>
2020-07-12 00:02:18 -0300

    Basic route for appointments & model

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express, { request, response } from 'express';
+import express from 'express';
 import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>
-  response.json({ message: 'We are connected' }),
-);
+app.use(express.json());
+app.use('/', routes);
 
 app.listen(3333, () => {
   console.log(
