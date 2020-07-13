2013c582fe762a4678ab1c0b3e7b437d0c86df08
Allan Melo <allmelo@users.noreply.github.com>
2020-07-13 02:15:40 -0300

    Database: routes & repository api

--- src/database/migrations/1594608975107-CreateAppointments.ts
+++ src/database/migrations/1594608975107-CreateAppointments.ts
@@ -11,6 +11,7 @@ export default class CreateAppointments1594608975107
           type: 'varchar',
           isPrimary: true,
           generationStrategy: 'uuid',
+          default: 'uuid_generate_v4()',
         },
         {
           name: 'provider',
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,25 +1,23 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentRepository = new AppointmentRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentRepository);
+  const appointments = await appointmentsRepository.find();
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
     const parsedDate = parseISO(date);
-
-    const createAppointment = new CreateAppointmentService(
-      appointmentRepository,
-    );
-    const appointment = createAppointment.execute({
+    const createAppointment = new CreateAppointmentService();
+    const appointment = await createAppointment.execute({
       date: parsedDate,
       provider,
     });
