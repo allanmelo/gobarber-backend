e4d23ea7aeb435a2298c1dbcfc04a800c8eba0b0
Allan Melo <allmelo@users.noreply.github.com>
2020-08-09 16:34:53 -0300

    New Error Class

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 import User from '../models/Users';
+import AppError from '../errors/AppError';
 
 interface Request {
   name: string;
@@ -15,7 +16,7 @@ class CreateUserService {
     const checkUserExists = await usersRepository.findOne({ where: { email } });
 
     if (checkUserExists) {
-      throw new Error('Email address already in use.');
+      throw new AppError('Email address already in use.');
     }
 
     const hashedPassword = await hash(password, 8);
